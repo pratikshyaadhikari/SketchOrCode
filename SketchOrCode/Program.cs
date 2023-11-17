804a199888a1e92c37866d39044d12337910eafa
pratikshyaadhikari <P.Adhikari9755@student.leedsbeckett.ac.uk>
2023-11-17 10:47:46 +0000

    global error handling

--- SketchOrCode/Program.cs
+++ SketchOrCode/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +17,30 @@ namespace SketchOrCode
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Add handler for UI thread exceptions
+            Application.ThreadException += new ThreadExceptionEventHandler(UIThreadException);
+
+            // Force all WinForms errors to go through handler
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            // This handler is for catching non-UI thread exceptions
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             Application.Run(new Form1());
+        }
+
+        private static void CurrentDomain_UnhandledException(Object sender, UnhandledExceptionEventArgs e)
+        {
+
+            Exception ex = (Exception)e.ExceptionObject;
+            MessageBox.Show(ex.Message);
+        }
+
+        private static void UIThreadException(object sender, ThreadExceptionEventArgs t)
+        {
+            MessageBox.Show(t.Exception.Message);
+
         }
     }
 }
