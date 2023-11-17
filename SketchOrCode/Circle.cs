13c3eba6182acee0246fbb574fa38a944f2330a4
pratikshyaadhikari <P.Adhikari9755@student.leedsbeckett.ac.uk>
2023-11-17 10:33:54 +0000

    working with circle class

--- SketchOrCode/Circle.cs
+++ SketchOrCode/Circle.cs
@@ -1,21 +1,44 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SketchOrCode
 {
     internal class Circle : Shape
     {
-        int radius;
-        public Circle(Color colour, int radius, int x, int y) : base(colour, x, y)
+        public Circle(Graphics graphics, bool isFillOn, Color color, int xPos, int yPos, List<string> parameter) : base(graphics, isFillOn, color, xPos, yPos, parameter)
         {
-            this.radius = radius;
         }
 
-        public override void draw(Graphics g)
+        public override void Validate()
         {
-            Pen p = new Pen(Color.Black, 2);
-            SolidBrush brush = new SolidBrush(Color.Black);
-            g.FillEllipse(brush, x, y, radius, 150);
-            g.DrawEllipse(p, x, y, radius, 150);
+            if (parameterList.Count != 1)
+            {
+                throw new SketchApplicationException("Circle param error");
+            }
+
+            Boolean isNumeric = int.TryParse(parameterList[0], out _);
+            if (!isNumeric)
+            {
+                throw new SketchApplicationException("Circle param value is not a number.");
+            }
+        }
+
+        public override void Draw()
+        {
+            int radius = int.Parse(parameterList[0]);
+
+            if (isFillOn)
+            {
+                Graphics.FillEllipse(new SolidBrush(color), xPos - radius, yPos - radius, 2 * radius, 2 * radius);
+            }
+            else
+            {
+                Graphics.DrawEllipse(new Pen(color), xPos - radius, yPos - radius, 2 * radius, 2 * radius);
+            }
         }
     }
 }
