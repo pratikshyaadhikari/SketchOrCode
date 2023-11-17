4e78a87ce0172a25184f5e45b4a85c5fbb526dda
pratikshyaadhikari <P.Adhikari9755@student.leedsbeckett.ac.uk>
2023-11-17 10:30:35 +0000

    exception class created.

---
+++ SketchOrCode/SketchApplicationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SketchOrCode
+{
+    internal class SketchApplicationException : Exception
+    {
+        public SketchApplicationException(string message) : base(message)
+        {
+
+        }
+        public SketchApplicationException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
