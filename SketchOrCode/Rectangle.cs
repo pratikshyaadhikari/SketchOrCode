20dbb7e967ec3eb463b92a78fd2d436d96f66c44
pratikshyaadhikari <P.Adhikari9755@student.leedsbeckett.ac.uk>
2023-11-17 10:34:12 +0000

    worked with rectangle class

--- SketchOrCode/Rectangle.cs
+++ SketchOrCode/Rectangle.cs
@@ -1,20 +1,53 @@
-﻿namespace SketchOrCode
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SketchOrCode
 {
-    internal class Rectangle : Shape
+    internal class Rectangle:Shape
     {
         protected int width, height;
-        public Rectangle(int width, int height)
+        public Rectangle(Graphics graphics, bool isFillOn, Color color, int xPos, int yPos, List<string> parameter) : base(graphics, isFillOn, color, xPos, yPos, parameter)
         {
-            this.width = width;// different from shape
-            this.height = height;
         }
-        private override void draw(Graphics g)
+        public override void Validate()
         {
-            Pen p = new Pen(Color.Black, 2);
-            SolidBrush brush = new SolidBrush(Color.Black);
-            g.FillRectangle(brush, x, y, width, height);
-            g.DrawRectangle(p, x, y, width, height);
+            if (parameterList.Count != 2)
+            {
+                throw new SketchApplicationException("Rectangle param error");
+            }
+
+            Boolean isNumeric1 = int.TryParse(parameterList[0], out _);
+            if (!isNumeric1)
+            {
+                throw new SketchApplicationException("Rectangle param first value is not a number.");
+            }
+
+            Boolean isNumeric2 = int.TryParse(parameterList[1], out _);
+            if (!isNumeric2)
+            {
+                throw new SketchApplicationException("Rectangle param second value is not a number.");
+            }
         }
+
+        public override void Draw()
+        {
+            int Length = int.Parse(parameterList[0]);
+            int Width = int.Parse(parameterList[1]);
+
+            if (isFillOn)
+            {
+                Graphics.FillRectangle(new SolidBrush(color), xPos, yPos, Length, Width);
+            }
+            else
+            {
+                Graphics.DrawRectangle(new Pen(color), xPos, yPos, Length, Width);
+            }
+        }
+
     }
 
 }
