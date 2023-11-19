b353eca5a8dd79e3789e96a8d64e629452a7d0be
pratikshyaadhikari <P.Adhikari9755@student.leedsbeckett.ac.uk>
2023-11-19 13:02:22 +0000

    worked with shape class and completed

--- SketchOrCode/Shape.cs
+++ SketchOrCode/Shape.cs
@@ -28,11 +28,31 @@ namespace SketchOrCode
 
         public virtual void Validate()
         {
+            if (parameterList.Count != 2)
+            {
+                throw new SketchApplicationException("DrawTo param error");
+            }
+
+            Boolean isNumeric1 = int.TryParse(parameterList[0], out _);
+            if (!isNumeric1)
+            {
+                throw new SketchApplicationException("DrawTo param first value is not a number.");
+            }
+
+            Boolean isNumeric2 = int.TryParse(parameterList[1], out _);
+            if (!isNumeric2)
+            {
+                throw new SketchApplicationException("DrawTo param second value is not a number.");
+            }
 
         }
 
         public virtual void Draw()
         {
+            int xPoint = int.Parse(parameterList[0]);
+            int yPoint = int.Parse(parameterList[1]);
+
+            Graphics.DrawLine(new Pen(color), new PointF(xPos, yPos), new PointF(xPoint, yPoint));
 
         }
     }
