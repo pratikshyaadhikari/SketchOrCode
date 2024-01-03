d18882b0388bbd0d5e105dc6bcd3d498586c3825
pratikshyaadhikari <P.Adhikari9755@student.leedsbeckett.ac.uk>
2024-01-03 17:24:29 +0000

    Loop test completed

---
+++ GPLUnitTest/LoopTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SketchOrCode.nUnitTests
+{
+    internal class LoopTest
+    {
+
+        CommandParser commandParser;
+
+        [SetUp]
+        public void Setup()
+        {
+            commandParser = new CommandParser(null);
+
+        }
+
+        [Test]
+        public void validCmdTest()
+        {
+            String command = "clear\r\n" +
+                "a= 0\r\n" +
+                "while a<500\r\n   " +
+                "a= a+5\r\n " +
+                "circle a\r\n" +
+                "endwhile";
+            commandParser.ParseCommand(command, null, true);
+        }
+
+    }
+}
